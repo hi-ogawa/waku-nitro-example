c5c6f11b92ea2751586e1e308b15deb61032cb2a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-24 15:34:34 +0900

    chore: update deps + use @tailwind/vite + optimize fonts

--- waku.config.ts
+++ waku.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "waku/config"
 import nitro from "@hiogawa/vite-plugin-nitro";
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   vite: {
     plugins: [
+      tailwindcss(),
       nitro({
         server: {
           environmentName: "rsc",
